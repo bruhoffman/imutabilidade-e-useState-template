12e58b4ec28535fd311bda26ac074f134167d3ab
bruhoffman <bruhoffman@gmail.com>
2024-02-28 17:22:23 -0300

    Exercícios da aula

--- src/Componentes/Garagem/Garagem.js
+++ src/Componentes/Garagem/Garagem.js
@@ -1,21 +1,30 @@
-import  Carro  from '../Carro/Carro'
-import { Botao, Estacionamento, GaragemContainer } from './styles'
+import { useState } from "react";
+import Carro from "../Carro/Carro";
+import { Botao, Estacionamento, GaragemContainer } from "./styles";
 
-export default function Garagem({ nome }) {
+export default function Garagem({ nome, alterarNome }) {
+  const [automovel, setAutomovel] = useState({
+    modelo: "HR-V",
+    cor: "Prata",
+    ano: 2024,
+    flex: true,
+    adicionadoPor: "Juliano",
+  });
+
   return (
     <GaragemContainer>
-      <h1>Garagem da {nome}</h1>
-      <Botao>Muda nome</Botao>
+      <h1>Garagem {nome}</h1>
+      <Botao onClick={() => alterarNome()}>Muda nome</Botao>
 
       <Estacionamento>
         <Carro
-          modelo="Corsa"
-          cor="branco"
-          ano={2020}
-          adicionadoPor="Labenu"
-          flex
+          modelo={automovel.modelo}
+          cor={automovel.cor}
+          ano={automovel.ano}
+          adicionadoPor={automovel.adicionadoPor}
+          flex={automovel.flex}
         />
       </Estacionamento>
     </GaragemContainer>
-  )
+  );
 }
